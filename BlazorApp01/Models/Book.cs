b331dee72e6aa7067719afda9fb09bee8d56d85d
Tony <122675474+Noidan@users.noreply.github.com>
2023-11-09 17:51:25 +0300

    create MOCK Database

---
+++ BlazorApp01/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorApp01.Models
+{
+    public class Book
+    {
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+    }
+}
